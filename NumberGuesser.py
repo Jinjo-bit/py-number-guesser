f6a90ad5b95cc516eb9184e14df1c7b5433a4985
Jinjo-bit <71643734+Jinjo-bit@users.noreply.github.com>
2021-08-30 09:35:56 -0400

    Add files via upload

---
+++ NumberGuesser.py
@@ -0,0 +1,26 @@
+from random import randint
+
+secret_number = randint(1, 1000)
+guess = int(input("Guess a number between 1 and 1000:"))
+
+guess_count = 1
+while secret_number != guess:
+   if guess > secret_number:
+      guess = int(input("Guess a lower number: "))
+   elif guess < secret_number:
+      guess = int(input("Guess a higher number: "))
+   guess_count += 1
+
+   print(f"Yay! You won in {guess_count} turns!")
+
+   def play_game():
+    secret_number = randint(1, 1000)
+   guess = int(input("Guess a number between 1 and 1000:"))
+   guess_count = 1
+   while secret_number != guess:
+      if guess > secret_number:
+         guess = int(input("Guess a lower number: "))
+      elif guess < secret_number:
+         guess = int(input("Guess a higher number: "))
+      guess_count += 1
+print(f"Yay! You won in {guess_count} turns!")
